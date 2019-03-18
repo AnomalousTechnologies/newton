b37fa382e876b3566293be9c7c4759507f5909b4
Matt Davies <matt@teamdavies.co.uk>
2019-03-18 18:47:08 +0000

    add storybook config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,9 @@
 import { configure } from '@storybook/react';
+import { addDecorator } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
+import { withThemesProvider } from 'storybook-addon-emotion-theme';
+
+import theme from '../src/theme';
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /.stories.tsx$/);
@@ -7,3 +12,6 @@ function loadStories() {
 }
 
 configure(loadStories, module);
+
+addDecorator(withInfo({ header: false }));
+addDecorator(withThemesProvider([theme]));
--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,45 @@
 module.exports = {
-  clearMocks: true,
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  setupFilesAfterEnv: ['<rootDir>tests/setupTests.ts'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!**/node_modules/**',
+  ],
+  transform: {
+    '.js': 'babel-jest',
+    '.(ts|tsx)': 'ts-jest',
+  },
+  transformIgnorePatterns: [
+    '<rootDir>/node_modules/(?!node-formatter)',
+  ],
+  testMatch: null,
+  testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/templates/',
+    '/.cache',
+  ],
+  moduleFileExtensions: [
+    'ts',
+    'tsx',
+    'js',
+  ],
+  moduleNameMapper: {
+    ".+\\.(css|styl|less|sass|scss)$": `identity-obj-proxy`,
+    ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": '<rootDir>/__mocks__/file.js',
+  },
   collectCoverage: true,
-  coverageDirectory: "coverage",
-  coveragePathIgnorePatterns: [
-    "/node_modules/"
-  ],
   coverageReporters: [
-    "text",
-    "lcov",
+    'lcov',
+    'html',
   ],
-  rootDir: "tests",
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    },
+  }
 };
