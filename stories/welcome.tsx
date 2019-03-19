249f5437e439b43c6bf5f25e01dcf3c0f29e1ec9
Matt Davies <matt@teamdavies.co.uk>
2019-03-19 18:17:59 +0000

    update welcome

--- stories/welcome.tsx
+++ stories/welcome.tsx
@@ -1,39 +1,70 @@
 import React from 'react';
 
+import styled from '@emotion/styled';
+
+import Code from '../src/bolts/code';
+import H1 from '../src/bolts/h1';
+import H2 from '../src/bolts/h2';
+import Link from '../src/bolts/link';
+import ListItem from '../src/bolts/list-item';
+import List from '../src/bolts/list-unordered';
+import P from '../src/bolts/p';
+import Strong from '../src/bolts/strong';
+import Sup from '../src/bolts/sup';
+import { Wrapper } from './utilities';
+
+const Title = styled(H1)`
+  margin-top: 0;
+`;
+
 const Welcome = () => (
-  <div>
-    <h1>Newton by Anomalous</h1>
-    <p>Newton is a design system and component library create by Anomalous.
-      Newton provides the backbone of our Inspect ™ and Mission Control ™ applications.
-      Newton is written for <a href="https://reactjs.org/">React</a> and provides a range of components
-      to speed up frontend development.</p>
-    <p>We follow a component structure that is similar in principle to the concepts laid out by Brad Frost
-      in <a href="http://bradfrost.com/blog/post/atomic-web-design/">Atomic Design</a> although we use our own language
-      and the concept has been slightly simplified.</p>
-    <p>If you're building applications to live within the Anomalous ecosystem feel free to use Newton.</p>
+  <Wrapper style={{ maxWidth: '700px' }}>
+    <Title>Newton by Anomalous</Title>
+    <P>Newton is a design system and component library created by Anomalous.
+      This library provides the UI backbone of our Inspect<Sup>™</Sup> and
+      Mission Control<Sup>™</Sup> applications.
+      Newton has been written for <Link href="https://reactjs.org/">React</Link> and provides a range of components
+      to speed up frontend development.</P>
+    <P>We follow a component structure that is similar in principle to the concepts laid out by Brad Frost
+      in <Link href="http://bradfrost.com/blog/post/atomic-web-design/">Atomic Design</Link> although
+      we use our own language and the concept has been slightly simplified.</P>
+    <P>We also use a stripped down version of <Link href="https://necolas.github.io/normalize.css/">Normalize</Link> to
+      encourage consistency across browsers, you can include our version of normalise or the original
+      (or none at all).</P>
+    <P>If you're building applications to live within the Anomalous ecosystem feel free to use Newton.</P>
 
-    <h2>Install</h2>
-    <p>With NPM:</p>
-    <code>$ npm install @anomalous/newton</code>
-    <p>With Yarn:</p>
-    <code>$ yarn add @anomalous/newton</code>
+    <H2>Install</H2>
+    <P>With NPM:</P>
+    <Code>$ npm install @anomalous/newton</Code>
+    <P>With Yarn:</P>
+    <Code>$ yarn add @anomalous/newton</Code>
 
-    <h2>Structure</h2>
-    <p>Newton provides the following types of component:</p>
-    <ul>
-      <li><b>Bolts:</b> quite literally the nuts and bolts of an application; the smallest units we provide.</li>
-      <li><b>Parts:</b> made up over several parts to create a more meaningful whole;
-        a small collection of components.</li>
-      <li><b>Vehicles:</b> made of parts and bolts representing a significant piece of functionality;
-        the building blocks of an app.</li>
-    </ul>
-    <p>In our applications we built on these components with the following:</p>
-    <ul>
-      <li><b>Layouts:</b> made up of a combination of bolts, parts, and vehicles to give pages structure.</li>
-      <li><b>Pages:</b> the final level representing a working feature, usually implementing a layout
-        and adding additional bolts, parts of vehicles.</li>
-    </ul>
-  </div>
+    <H2>Structure</H2>
+    <P>Newton provides the following types of component:</P>
+    <List>
+      <ListItem>
+        <Strong>Bolts:</Strong> quite literally the nuts and bolts of an application; the smallest units we provide.
+      </ListItem>
+      <ListItem>
+        <Strong>Parts:</Strong> made up over several parts to create a more meaningful whole;
+          a small collection of components.
+      </ListItem>
+      <ListItem>
+        <Strong>Vehicles:</Strong> made of parts and bolts representing a significant piece of functionality;
+          the building blocks of an app.
+      </ListItem>
+    </List>
+    <P>In our applications we built on these components with the following:</P>
+    <List>
+      <ListItem>
+        <Strong>Layouts:</Strong> made up of a combination of bolts, parts, and vehicles to give pages structure.
+      </ListItem>
+      <ListItem>
+        <Strong>Pages:</Strong> the final level representing a working feature, usually implementing a layout
+          and adding additional bolts, parts of vehicles.
+      </ListItem>
+    </List>
+  </Wrapper>
 );
 
 export default Welcome;
