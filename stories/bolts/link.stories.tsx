2cbb1459da0a31c94b6259f3612178b23e52788d
Matt Davies <matt@teamdavies.co.uk>
2019-03-19 18:20:57 +0000

    add link

---
+++ stories/bolts/link.stories.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { text, withKnobs } from '@storybook/addon-knobs';
+import { storiesOf } from '@storybook/react';
+
+import Link from '../../src/bolts/link';
+import P from '../../src/bolts/p';
+import { Wrapper } from '../utilities';
+
+const stories = storiesOf('Bolts|Text', module)
+  .addDecorator(withKnobs)
+  .addParameters({
+    info: { disable: true },
+    options: { showPanel: true },
+  });
+
+stories.add('Link', () => (
+  <Wrapper>
+    <P>This sentence has <Link>{text('Content', 'an example of a link')}</Link> placed within it.</P>
+  </Wrapper>
+));
---
+++ tests/bolts/link.test.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { mount, shallow } from 'enzyme';
+import withTheme from '../withTheme';
+
+import Link from '../../src/bolts/link';
+
+describe('Bolt: Link', () => {
+  it('Can render link', () => {
+    const wrapper = shallow(
+      <div>
+        <Link>My Link</Link>
+      </div>,
+    );
+    expect(wrapper.find(Link).text()).toContain('My Link');
+    // @ts-ignore
+    expect(wrapper.find(Link).type().__emotion_base).toEqual('a');
+  });
+
+  it('Can mount with theme', () => {
+    const wrapper = mount(withTheme(<Link>My Link</Link>));
+    expect(wrapper.find(Link).text()).toEqual('My Link');
+  });
+});
