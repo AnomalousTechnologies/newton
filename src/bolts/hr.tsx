d4a6d2f9cb79b51f3fc084b791c8ad6a4656abc6
Matt Davies <matt@teamdavies.co.uk>
2019-03-19 18:19:51 +0000

    add hard rule

---
+++ src/bolts/hr.tsx
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+export default styled.hr`
+  box-sizing: content-box;
+  margin: 1rem 0;
+  height: 0;
+  overflow: visible;
+  border: 0;
+  border-top: 1px solid rgba(0,0,0,.2);
+`;
---
+++ tests/bolts/hr.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { shallow } from 'enzyme';
+
+import HardRule from '../../src/bolts/hr';
+
+describe('Bolt: HardRule', () => {
+  it('Can render hr', () => {
+    const wrapper = shallow(
+      <div>
+        <HardRule />
+      </div>,
+    );
+    // @ts-ignore
+    expect(wrapper.find(HardRule).type().__emotion_base).toEqual('hr');
+  });
+});
