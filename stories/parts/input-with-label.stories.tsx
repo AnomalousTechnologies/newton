069fdde3ca51bcef4b06878e9748d00c11044970
Matt Davies <matt@teamdavies.co.uk>
2019-03-19 18:24:21 +0000

    update input with label

--- stories/parts/input-with-label.stories.tsx
+++ stories/parts/input-with-label.stories.tsx
@@ -5,28 +5,33 @@ import { text, withKnobs } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react';
 
 import InputWithLabel from '../../src/parts/input-with-label';
+import { Wrapper } from '../utilities';
 
 const stories = storiesOf('Parts|Input With Label', module)
   .addDecorator(withKnobs)
   .addParameters({ options: { showPanel: true } });
 
 stories.add('Input', () => (
-  <InputWithLabel
-    label={text('Label', 'My Input')}
-    name="foo"
-    onChange={action('changed')}
-    placeholder={text('Placeholder', 'Enter a value')}
-    value={text('Value', 'Hello, friend')}
-  />
+  <Wrapper>
+    <InputWithLabel
+      label={text('Label', 'My Input')}
+      name="foo"
+      onChange={action('changed')}
+      placeholder={text('Placeholder', 'Enter a value')}
+      value={text('Value', 'Hello, friend')}
+    />
+  </Wrapper>
 ));
 
 stories.add('Textarea', () => (
-  <InputWithLabel
-    label={text('Label', 'My Input')}
-    name="foo"
-    onChange={action('changed')}
-    placeholder={text('Placeholder', 'Enter a value')}
-    type="textarea"
-    value={text('Value', 'Hello, friend')}
-  />
+  <Wrapper>
+    <InputWithLabel
+      label={text('Label', 'My Input')}
+      name="foo"
+      onChange={action('changed')}
+      placeholder={text('Placeholder', 'Enter a value')}
+      type="textarea"
+      value={text('Value', 'Hello, friend')}
+    />
+  </Wrapper>
 ));
