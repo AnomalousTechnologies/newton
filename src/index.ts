49558a6e90d105ffd842b7fe9ffd9e16bf923dfc
Matt Davies <matt@teamdavies.co.uk>
2019-03-20 10:02:24 +0000

    add all components to index export

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,54 @@
 import Button from './bolts/button';
+import Code from './bolts/code';
+import H1 from './bolts/h1';
+import H2 from './bolts/h2';
+import H3 from './bolts/h3';
+import H4 from './bolts/h4';
+import Hr from './bolts/hr';
+import Img from './bolts/img';
 import Input from './bolts/input';
 import Label from './bolts/label';
+import Link from './bolts/link';
+import ListItem from './bolts/list-item';
+import ListOrdered from './bolts/list-ordered';
+import ListUnordered from './bolts/list-unordered';
+import Optgroup from './bolts/optgroup';
+import Option from './bolts/option';
+import P from './bolts/p';
+import Pre from './bolts/pre';
+import Select from './bolts/select';
+import Small from './bolts/small';
+import Strong from './bolts/strong';
+import Sub from './bolts/sub';
+import Sup from './bolts/sup';
 import Textarea from './bolts/textarea';
 
-import InputWithLabel from './bolts/label';
+import InputWithLabel from './parts/input-with-label';
 
 export {
   Button,
+  Code,
+  H1,
+  H2,
+  H3,
+  H4,
+  Hr,
+  Img,
   Input,
   Label,
+  Link,
+  ListItem,
+  ListOrdered,
+  ListUnordered,
+  Optgroup,
+  Option,
+  P,
+  Pre,
+  Select,
+  Small,
+  Strong,
+  Sub,
+  Sup,
   Textarea,
   InputWithLabel,
 };
