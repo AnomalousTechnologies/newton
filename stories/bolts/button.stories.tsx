09611f015b5a13da93087f76f5891bdc25cbe212
Matt Davies <matt@teamdavies.co.uk>
2019-05-24 12:03:26 +0100

    adjust button stories

--- stories/bolts/button.stories.tsx
+++ stories/bolts/button.stories.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 
 import styled from '@emotion/styled';
 import { action } from '@storybook/addon-actions';
-import { text, withKnobs } from '@storybook/addon-knobs';
+import { boolean, select, text, withKnobs } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react';
 
-import Button from '../../src/bolts/button';
-import Code from '../../src/bolts/code';
-import H1 from '../../src/bolts/h1';
-import H2 from '../../src/bolts/h2';
+import UserIcon from '../../src/bolts/icons/large/user';
+import Button, { ButtonSize, ButtonType } from '../../src/bolts/inputs/button';
+import Code from '../../src/bolts/typography/code';
+import H1 from '../../src/bolts/typography/h1';
+import H2 from '../../src/bolts/typography/h2';
 import { Wrapper } from '../utilities';
 
 const Title = styled(H1)`
@@ -20,10 +21,112 @@ storiesOf('Bolts|Button', module)
   .addParameters({
     options: { showPanel: true },
   })
+  .add('Large', () => (
+    <Wrapper>
+      <Title>Button</Title>
+      <Button onClick={action('clicked')} size={ButtonSize.LARGE}>
+        {text('Placeholder', 'Large Button')}
+      </Button>
+      <H2>Source Code</H2>
+      <Code>
+        &lt;Button onClick=&#123;...&#125; size=&#123;...&#125;&gt;...&lt;/Button&gt;
+      </Code>
+    </Wrapper>
+  ))
+  .add('Small', () => (
+    <Wrapper>
+      <Title>Button</Title>
+      <Button onClick={action('clicked')} size={ButtonSize.SMALL}>
+        {text('Placeholder', 'Small Button')}
+      </Button>
+      <H2>Source Code</H2>
+      <Code>
+        &lt;Button onClick=&#123;...&#125; size=&#123;...&#125;&gt;...&lt;/Button&gt;
+      </Code>
+    </Wrapper>
+  ))
+  .add('Icon', () => (
+    <Wrapper>
+      <Title>Button</Title>
+      <Button
+        icon={<UserIcon />}
+        onClick={action('clicked')}
+        size={select('Size', [ButtonSize.SMALL, ButtonSize.LARGE], ButtonSize.SMALL)}
+      >
+        {text('Placeholder', 'Icon Button')}
+      </Button>
+      <H2>Source Code</H2>
+      <Code>
+        &lt;Button icon=&#123;...&#125; onClick=&#123;...&#125; size=&#123;...&#125;&gt;...&lt;/Button&gt;
+      </Code>
+    </Wrapper>
+  ))
   .add('Primary', () => (
     <Wrapper>
       <Title>Button</Title>
-      <Button onClick={action('clicked')}>{text('Placeholder', 'My Button')}</Button>
+      <Button onClick={action('clicked')}>
+        {text('Placeholder', 'My Button')}
+      </Button>
+      <H2>Source Code</H2>
+      <Code>
+        &lt;Button onClick=&#123;...&#125;&gt;...&lt;/Button&gt;
+      </Code>
+    </Wrapper>
+  ))
+  .add('Secondary', () => (
+    <Wrapper>
+      <Title>Button</Title>
+      <Button onClick={action('clicked')} type={ButtonType.SECONDARY}>
+        {text('Placeholder', 'My Button')}
+      </Button>
+      <H2>Source Code</H2>
+      <Code>
+        &lt;Button onClick=&#123;...&#125; type=&#123;...&#125;&gt;...&lt;/Button&gt;
+      </Code>
+    </Wrapper>
+  ))
+  .add('Success', () => (
+    <Wrapper>
+      <Title>Button</Title>
+      <Button onClick={action('clicked')} type={ButtonType.SUCCESS}>
+        {text('Placeholder', 'My Button')}
+      </Button>
+      <H2>Source Code</H2>
+      <Code>
+        &lt;Button onClick=&#123;...&#125; type=&#123;...&#125;&gt;...&lt;/Button&gt;
+      </Code>
+    </Wrapper>
+  ))
+  .add('Warning', () => (
+    <Wrapper>
+      <Title>Button</Title>
+      <Button onClick={action('clicked')} type={ButtonType.WARNING}>
+        {text('Placeholder', 'My Button')}
+      </Button>
+      <H2>Source Code</H2>
+      <Code>
+        &lt;Button onClick=&#123;...&#125; type=&#123;...&#125;&gt;...&lt;/Button&gt;
+      </Code>
+    </Wrapper>
+  ))
+  .add('Danger', () => (
+    <Wrapper>
+      <Title>Button</Title>
+      <Button onClick={action('clicked')} type={ButtonType.DANGER}>
+        {text('Placeholder', 'My Button')}
+      </Button>
+      <H2>Source Code</H2>
+      <Code>
+        &lt;Button onClick=&#123;...&#125; type=&#123;...&#125;&gt;...&lt;/Button&gt;
+      </Code>
+    </Wrapper>
+  ))
+  .add('Loading', () => (
+    <Wrapper>
+      <Title>Button</Title>
+      <Button isLoading={boolean('Loading', true)} onClick={action('clicked')}>
+        {text('Placeholder', 'Loading...')}
+      </Button>
       <H2>Source Code</H2>
       <Code>
         &lt;Button onClick=&#123;...&#125;&gt;...&lt;/Button&gt;
