a3f2a61087f494b0f91ae6b18043dc19f308bc8c
Matt Davies <matt@teamdavies.co.uk>
2019-05-24 12:03:33 +0100

    add avatar bolt

---
+++ src/bolts/avatars/avatar.tsx
@@ -0,0 +1,79 @@
+import React from 'react';
+
+import styled from '@emotion/styled';
+import PropTypes from 'prop-types';
+
+import UserIcon from '../icons/large/user';
+
+export enum AvatarSize {
+  SMALL = '32px',
+  LARGE = '64px',
+}
+
+interface AvatarProps {
+  name: string;
+  onClick?: () => void;
+  size?: AvatarSize;
+  src?: string;
+}
+
+interface MaskProps {
+  className?: string;
+  children: any;
+  onClick?: () => void;
+  size?: AvatarSize;
+}
+
+const Mask = ({
+  className,
+  children,
+  onClick,
+}: MaskProps) => (
+  <div className={className} onClick={onClick} role={onClick ? 'button' : 'img'}>
+    {children}
+  </div>
+);
+
+Mask.defaultProps = {
+  size: AvatarSize.LARGE,
+};
+
+const MaskStyled = styled(Mask)`
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  width: ${(props) => props.size};
+  height: ${(props) => props.size};
+  overflow: hidden;
+  border-radius: 50%;
+  background-color: ${(props) => props.theme.color.select.tonal[1]};
+  transition: width 1s, height 1s;
+
+  img {
+    width: 100%;
+    height: 100%;
+  }
+
+  svg {
+    width: 60%;
+    height: 60%;
+
+    path {
+      fill: ${(props) => props.theme.color.select.tonal[4]};
+    }
+  }
+`;
+
+const Avatar = ({ name, onClick, size, src }: AvatarProps) => {
+  const userImage = <img src={src} alt={name} title={name} />;
+  const placeholderImage = <UserIcon title={name} />;
+  return <MaskStyled onClick={onClick} size={size}>{src ? userImage : placeholderImage}</MaskStyled>;
+};
+
+Avatar.propTypes = {
+  name: PropTypes.string.isRequired,
+  size: PropTypes.string,
+  src: PropTypes.string,
+};
+
+export default Avatar;
---
+++ stories/bolts/avatars.stories.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+
+import styled from '@emotion/styled';
+import { select, text, withKnobs } from '@storybook/addon-knobs';
+import { storiesOf } from '@storybook/react';
+
+import Avatar, { AvatarSize } from '../../src/bolts/avatars/avatar';
+import Code from '../../src/bolts/typography/code';
+import H1 from '../../src/bolts/typography/h1';
+import H2 from '../../src/bolts/typography/h2';
+import { Wrapper } from '../utilities';
+
+const Title = styled(H1)`
+  margin-top: 0;
+`;
+
+storiesOf('Bolts|Avatar', module)
+  .addDecorator(withKnobs)
+  .addParameters({
+    options: { showPanel: true },
+  })
+  .add('Default', () => (
+    <Wrapper>
+      <Title>Avatar Default</Title>
+      <Avatar
+        name={text('Name', 'Matt Davies')}
+        size={select('Size', [AvatarSize.SMALL, AvatarSize.LARGE], AvatarSize.LARGE)}
+      />
+      <H2>Source Code</H2>
+      <Code>
+        &lt;Avatar name=&#123;...&#125; src=&#123;...&#125; /&gt;
+      </Code>
+    </Wrapper>
+  ))
+  .add('Large', () => (
+    <Wrapper>
+      <Title>Avatar Large</Title>
+      <Avatar
+        name={text('Name', 'Phyllis Thompson')}
+        src="https://randomuser.me/api/portraits/women/59.jpg"
+      />
+      <H2>Source Code</H2>
+      <Code>
+        &lt;Avatar name=&#123;...&#125; src=&#123;...&#125; /&gt;
+      </Code>
+    </Wrapper>
+  ))
+  .add('Small', () => (
+    <Wrapper>
+      <Title>Avatar Small</Title>
+      <Avatar
+        name={text('Name', 'Kevin Torres')}
+        size={AvatarSize.SMALL}
+        src="https://randomuser.me/api/portraits/men/92.jpg"
+      />
+      <H2>Source Code</H2>
+      <Code>
+        &lt;Avatar name=&#123;...&#125; src=&#123;...&#125; /&gt;
+      </Code>
+    </Wrapper>
+  ));
