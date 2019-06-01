d20a5db807b5727dca2cf05d4ae2e5a6c328313e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-01 17:08:09 +0000

    Deploy AnomalousTechnologies/newton to github.com/AnomalousTechnologies/newton.git:gh-pages

--- main.eb056c7b0aace24887f7.bundle.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{400:function(n,o,p){p(401),p(477),n.exports=p(868)},477:function(n,o,p){"use strict";p.r(o);p(478),p(703),p(858),p(861)}},[[400,1,2]]]);
